8648349eb48a72e0823a7a62cc5985526648db66
Jose Antonio Torres Garibay <pepe_torres1998@hotmail.com>
2019-06-01 03:43:54 -0500

    Add log in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,6 +38,15 @@ class UsersController < ApplicationController
     @user.destroy
   end
 
+  def log_in
+    user = User.find_by(email: params[:email])
+    if !user.nil? && user.authenticate(params[:password])
+      render json: { logged: true }
+    else
+      render json: { logged: false }
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :sessions
   resources :users
+
+  get '/log_in', to: 'users#log_in'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
